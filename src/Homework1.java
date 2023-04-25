8a18cfcd9afe7cad0883f9c0b1ab0839f550b23e
JulWas <julia.wasilewska.18@gmail.com>
2023-04-25 19:13:57 +0200

    my initial commit

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,6 +1,36 @@
 public class Homework1 {
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        // Этот код выводит сообщение, one-line comment
+        // Legacy Code - код написанный другим программистом и скорее всего
+        // на старой версии языка
+        // Java - это чаще всего 1.8 (Java 8)
+
+        System.out.print("");
+        System.out.println();
+
+        // new line - symbol, \n
+
+        System.out.print("Please, enter your name:"+"Julia");
+        System.out.println();
+        System.out.println("Please, enter your surname: Wasilewska");
+        System.out.println("Please, enter your ege:36");
+
+        System.out.println("Lat's calculate sume 5+1:"+(5+10));
+        System.out.println("Julia" + " " + "Wasilewska" +
+                ". Welcom to my First " +
+                        "Lesson to ........");
+        // This is an example comment for Git
+        System.out.println();
+        System.out.print("Please, enter your name & surname: ");
+        Scanner scanner = new Scanner(System.in);
+        Scanner.nextline();
+
+        // New Comment
+
+        String name = "123";
+        int age = 34;
+        double weight = 88.1;
+        char symbol = 'A';
     }
 }
