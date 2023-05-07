9c791e38b9592a36b791075d767f9daa00c21b21
JulWas <julia.wasilewska.18@gmail.com>
2023-05-07 10:08:05 +0200

    scanner is a class field

--- src/Homework1.java
+++ src/Homework1.java
@@ -1,66 +1,63 @@
 import java.util.Scanner;
 
 public class Homework1 {
-    public static void main(String[] args) {
+    static Scanner enter;
 
+    public static void main(String[] args) {
+        enter = new Scanner(System.in);
 
         ScannerMyParameters();
         System.out.println();
-
         CharToInt();
         System.out.println();
-
         calculateBim();
+        System.out.println();
+
+        enter.close();
+    }
+    public static void ScannerMyParameters() {
+
+        System.out.print("Please, enter your name :");
+        String name = enter.nextLine();
+
+        System.out.print("Please, enter your surname :");
+        String surname = enter.nextLine();
+
+        System.out.print("Please, enter your age :");
+        String age = enter.nextLine();
+
+        System.out.print("Please, enter your height :");
+        String height = enter.nextLine();
+
+        System.out.print("Please, enter your weight :");
+        String weight = enter.nextLine();
+
+        System.out.println("Name: " + name);
+        System.out.println("Sureame: " + surname);
+        System.out.println("Age: " + age);
+        System.out.println("Haight: " + height);
+        System.out.println("Weight: " + weight);
+
     }
 
-        public static void ScannerMyParameters() {
+    public static void CharToInt() {
 
-            Scanner enter = new Scanner(System.in);
-            System.out.print("Please, enter your name :");
-            String name = enter.nextLine();
+        char mySymbol = 'A';
+        System.out.println("A -" + (int) mySymbol);
+        char mySymbol1 = 'C';
+        System.out.println("C -" + (int) mySymbol1);
 
-            System.out.print("Please, enter your surname :");
-            String surname = enter.nextLine();
+    }
 
-            System.out.print("Please, enter your age :");
-            String age = enter.nextLine();
+    public static void calculateBim() {
 
-            System.out.print("Please, enter your height :");
-            String height = enter.nextLine();
-
-            System.out.print("Please, enter your weight :");
-            String weight = enter.nextLine();
-
-            System.out.println("Name: " + name);
-            System.out.println("Sureame: " + surname);
-            System.out.println("Age: " + age);
-            System.out.println("Haight: " + height);
-            System.out.println("Weight: " + weight);
-
-            enter.close();
-        }
-        public static void CharToInt(){
-
-            char mySymbol ='A';
-            System.out.println("A -" + (int)mySymbol);
-            char mySymbol1 ='C';
-            System.out.println("C -" + (int)mySymbol1);
-
-        }
-
-    public static void calculateBim(){
-
-        Scanner bim = new Scanner(System.in);
         System.out.print("Please, enter your Weight :");
-        double Weight= bim.nextDouble();
+        double Weight = enter.nextDouble();
         System.out.print("Please, enter your Height :");
-        double Height = bim.nextDouble();
-        double BMI = Weight / (Height*Height);
+        double Height = enter.nextDouble();
+        double BMI = Weight / (Height * Height);
 
         System.out.println("BMI is " + BMI + " kg/m2");
 
-        bim.close();
-
     }
-
-        }
+}
