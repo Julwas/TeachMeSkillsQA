37746f269a2a9f252a47fa88d4f38974806cc1f8
JulWas <julia.wasilewska.18@gmail.com>
2023-05-12 14:02:30 +0200

    task3

--- src/Homework.java
+++ src/Homework.java
@@ -2,21 +2,23 @@ import java.util.Scanner;
 public class Homework {
     public static void main(String[] args) {
 
-     //  swithcOperator("8");
-      // ifSeasons(2);
+        // swithcOperator("8");
+        // ifSeasons(2);
         //evenOrOdd(13);
         //ifTemperature(5);
-       // rainbowCololr(2);
-       // forTask1();
-        //forTask2();
-        forTask3();
-        //forTask4();
+        // rainbowCololr(2);
+        //cycleTask1();
+        //cycleTask2();
+        //integerSum();
+        //cycleTask4();
+        //cycleTask5();
+        cycleTask6();
     }
-    //task1
-    public static void swithcOperator(String month){
 
-        switch(month)
-        {
+    //task1
+    public static void swithcOperator(String month) {
+
+        switch (month) {
             case "1":
             case "2":
             case "12":
@@ -37,104 +39,97 @@ public class Homework {
                 break;
         }
     }
+
     //task2
-    public static void ifSeasons(int month )
-    {
-        if (month > 9 && month < 12)
-        {
+    public static void ifSeasons(int month) {
+        if (month > 9 && month < 12) {
             System.out.println("This is :Autumn");
         }
-        if (month > 3 && month < 6)
-        {
+        if (month > 3 && month < 6) {
             System.out.println("This is :Spring");
         }
-        if (month > 5 && month < 9)
-        {
+        if (month > 5 && month < 9) {
             System.out.println("This is :Summer");
-        }
-        else
-        {
+        } else {
             System.out.println("This is :Winter");
         }
     }
-//task3
-    public static void  evenOrOdd(int a)
-    {
-        if((a%2)==0)
-        {
+
+    //task3
+    public static void evenOrOdd(int a) {
+        if ((a % 2) == 0) {
             System.out.println("This is : even");
-        }
-        else
-        {
+        } else {
             System.out.println("This is : odd");
         }
     }
+
     //task 4
-    public static void ifTemperature(int t )
-    {
-        if (t> -20 && t <= -5)
-        {
+    public static void ifTemperature(int t) {
+        if (t > -20 && t <= -5) {
             System.out.println("The weather is normal");
         }
-        if (t > -5)
-        {
+        if (t > -5) {
             System.out.println("The weather is warm");
-        }
-        else
-        {
+        } else {
             System.out.println("The weather is cold");
         }
     }
+
     //task 5
-    public static void rainbowCololr(int nColor )
-    {
+    public static void rainbowCololr(int nColor) {
         String color = (nColor == 1) ? "Red" : (nColor == 2) ? "Orange" :
-        (nColor == 3) ? "y" : (nColor == 4) ? "g" : (nColor == 5) ? "b" :
-                (nColor == 6) ? "db" : "purple";
+                (nColor == 3) ? "y" : (nColor == 4) ? "g" : (nColor == 5) ? "b" :
+                        (nColor == 6) ? "db" : "purple";
 
         System.out.println(color);
     }
-    //task1 cykly
+    //cycle
 
-    public static void forTask1()
-    {
-        for (int a = 1; a<=99; a++)
-        {
-            if( a % 2!=0)
-            {
-                System.out.println(--a);
+    public static void cycleTask1() {
+        for (int a = 1; a <= 99; a++) {
+            if (a % 2 != 0) {
+                System.out.println(a++);
             }
-        };
-
+        }
     }
     //task2
-    public static void forTask2()
-    {
-        for (int a = 5; a>=1; --a)
-                {
-                System.out.println(a);
-            }
+    public static void cycleTask2() {
+        for (int a = 5; a >= 1; --a) {
+            System.out.println(a);
+        }
     }
     //task 3 not done
-    public static void forTask3()
-    {
+    public static void integerSum() {
         Scanner nom = new Scanner(System.in);
-        System.out.println( "Enter number " );
+        System.out.println("Enter positive integer : ");
         int nomber = nom.nextInt();
-
-
+        int sum = (nomber * (nomber + 1)) / 2;
+        System.out.println(sum);
+        nom.close();
     }
-  //not done
-    public static void forTask4()
-    {
-        int n =7;
-        for(int i=0; i<13; i++)
-        {
+    //not done
+    public static void cycleTask4() {
+        int n = 7;
+        for (int i = 0; i < 13; i++) {
             System.out.println(n);
-            n+=7;
+            n += 7;
         }
-
     }
-
+    public static void cycleTask5() {
+        int n = 0;
+        for (int i = 0; i >= -9; --i) {
+            System.out.println(n);
+            n += 5;
+        }
+    }
+    public static void cycleTask6()
+    {
+        int a1 = 100;
+        for (int a = 11; a <= 21; a++)
+        {
+            System.out.println(a1);
+                a1 = a * a;
+        }
+    }
 }
-
